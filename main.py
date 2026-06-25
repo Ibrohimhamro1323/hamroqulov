9b2799f7950405db4c353c7cbc6077ea54bd5e08
Ibrohimhamro1323 <hamroqulovibrohim469@gmail.com>
2026-06-25 15:20:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Streamlit sahifasini to'liq kenglikda sozlash
-st.set_page_config(layout="wide", page_title="SportMart")
+st.set_page_config(
+    page_title="SportMart",
+    page_icon="💪",
+    layout="wide"
+)
 
-# indexw.html faylini o'qib olish va render qilish
-with open("indexw.html", "r", encoding="utf-8") as f:
+with open("index.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-# HTMLni Streamlit ichida ko'rsatish
-# height qiymatini sahifangiz uzunligiga qarab o'zgartirishingiz mumkin
-components.html(html_content, height=2500, scrolling=True)
+components.html(
+    html_content,
+    height=5000,
+    scrolling=True
+)
